dcef63346920f7810d5db29d4fc1b3ab94bcb98f
nuke <all.hail.robots@gmail.com>
2021-08-19 14:54:49 -0700

    Comment added

--- src/main/java/com/example/LaboratoryExercise4/InventoryController.java
+++ src/main/java/com/example/LaboratoryExercise4/InventoryController.java
@@ -1,5 +1,5 @@
 package com.example.LaboratoryExercise4;
-
+//https://github.com/Benderz/final300336001Andrey.git
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
